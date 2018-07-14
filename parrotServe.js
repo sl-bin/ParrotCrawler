5cc56cbf4887cc96afbaf997cf7f9820577f52d9
Christoph Rico <christophrico@Hackintosh.local>
2018-07-14 15:22:26 -0700

    Got parrotServe to accept POST in correct format

--- parrotServe.js
+++ parrotServe.js
@@ -11,65 +11,32 @@ app.use(cors());
 
 //------------  App Routes ------------//
 //recieve the POST search request from the frontend
-app.route('/api/search').post((req,res) => {
-  searchTerms = req.body;
+app.route('/api/search/').post((req,res) => {
+  console.log("here I am in node having just received the request");
+  var searchJSON = req.body;
 
-  console.log("POST search request recieved with values: " + searchTerms);
+  //for testing that JSON received is correct
+  var searchString = JSON.stringify(searchJSON);
+  console.log("POST search request recieved with values: " + searchString);
 
-  pyParrotCrawl(searchTerms);
+  var startURL = searchJSON.URL;
+  var nDepth = searchJSON.n;
+  var phrase = searchJSON.searchPhrase;
+  var type = searchJSON.searchType;
 
-  res.send(200, searchTerms);
+  console.log("startURL: " + startURL);
+  console.log("nDepth: " + nDepth);
+  console.log("searchPhrase: " + phrase);
+  console.log("searchType: " + type);
+
+  //pyParrotCrawl(searchTerms);
+
+  //send the search terms back to angular
+  res.status(200).send("Are you talking to ME?");
 });
 
 
-//listen on port 8000 for requests from front end
+//listen on port 8000 for requests
 app.listen(8000, () => {
   console.log('ParrotServe started on port http://localhost:8000!');
 });
-
-
-//------------  Python-Shell Function Call ------------//
-//function to call python-shell when search is recieved
-function pyParrotCrawl(searchTerms) {
-  //parse search terms out into individual variables
-  var startURL = searchTerms.URL;
-  var nDepth = searchTerms.n;
-  var searchPhrase = searchTerms.searchPhrase;
-  var type = searchTerms.searchType;
-
-  //choose the right script depending which search type is specified
-  var scriptToRun;
-  switch(type) {
-    case "BFS":
-      scriptToRun = "bfs.py";
-      break;
-    case "RDFS":
-      scriptToRun = "rdfs.py";
-      break;
-    case "TDFS":
-      scriptToRun = "tdfs.py";
-      break;
-    }
-
-  //set up pyshell for a run
-  var PythonShell = require('python-shell');
-  var options = {
-      mode: 'text',
-      pythonPath: '/usr/bin/python3',
-      pythonOptions: ['-u'],
-      scriptPath: 'path to script goes here'
-      args: [startURL, nDepth, searchPhrase]
-  };
-
-  //call crawl script and pass it the search terms
-  PythonShell.run(scriptToRun, options, function(err, results) {
-    if (err) throw err;
-  });
-
-  //get the data back out
-  PythonShell.receive(data) {
-    console.log("Here we are in parrotServe and the crawler results are: " + data);
-  }
-
-
-}
