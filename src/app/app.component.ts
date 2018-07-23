d43944a0f182da8cb25860678a2c7d0a5442e44c
CharlesLedbetter <32339141+CharlesLedbetter@users.noreply.github.com>
2018-07-22 23:55:41 -0500

    added more frontend error handling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -66,6 +66,38 @@ import { ParrotSearchService } from './parrot-search.service';
             ))
           ], { optional: true }),
         ])
+      ]),
+      transition('error => home', [
+        query(':enter, :leave', style({ position: 'fixed', height:'250%', width: '100%' })
+          , { optional: true }),
+        group([
+          query(':enter', [
+            style({ transform: 'translateY(100%)' }),
+            animate('1.5s ease-in-out', style({ transform: 'translateY(0%)' }))
+          ], { optional: true }),
+          query(':leave', [
+            style({ transform: 'translateY(0%)' }),
+            animate('1.5s ease-in-out', style(
+              { transform: 'translateY(0%)', opacity: 0 }
+            ))
+          ], { optional: true }),
+        ])
+      ]),
+      transition('* => error', [
+        query(':enter, :leave', style({ position: 'fixed', height:'250%', width: '100%' })
+          , { optional: true }),
+        group([
+          query(':enter', [
+            style({ transform: 'translateY(100%)' }),
+            animate('1.5s ease-in-out', style({ transform: 'translateY(0%)' }))
+          ], { optional: true }),
+          query(':leave', [
+            style({ transform: 'translateY(0%)' }),
+            animate('1.5s ease-in-out', style(
+              { transform: 'translateY(0%)', opacity: 0 }
+            ))
+          ], { optional: true }),
+        ])
       ])
     ])
    ],
