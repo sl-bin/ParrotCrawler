c626f40d2d8c0261037ae30ae4bb4c434c3882bf
Christoph Rico <christophrico@Hackintosh.local>
2018-07-28 19:33:22 -0700

    prevSearch.getFromLocal now returns array of ParrotSearch objs

--- src/app/prevSearch.service.ts
+++ src/app/prevSearch.service.ts
@@ -6,20 +6,16 @@ import { Inject, Injectable } from '@angular/core';
 import { LOCAL_STORAGE, StorageService } from 'angular-web_storage-service';
 import { ParrotSearch } from './parrot-search';
 
-var keyCount;
 
 //using this site as a reference: https://www.js-tutorials.com/javascript-tutorial/use-localstorage-sessionstorage-using-webstorage-angular4/
 @Injectable({ providedIn: 'root' })
 export class LocalStorageService {
 
   constructor(@Inject(LOCAL_STORAGE) private storage: StorageService) {
+    //if this is the first entry in localStorage
     //set the number of values stored in localStorage as key number 0
-    console.log("Here we are in prevSearch.service at the constructor");
-    console.log("Number of items in localStorage is " + String(this.storage.get("0")));
-
     if(!this.storage.get("0")){
-      console.log("Initializing storage space");
-      keyCount = 0;
+      console.log("Initializing localStorage");
       this.storage.set("0", "0");
     }
   }
@@ -28,33 +24,31 @@ export class LocalStorageService {
   //save function that accepts ParrotSearch objects to save to localstorage
   public saveInLocal(searchJSON: ParrotSearch): void {
     //save the search at the next index in the array
-    //var curKey = parseInt(this.storage.get("0"));
     var searchString = JSON.stringify(searchJSON);
+    var keyCount = parseInt(this.storage.get("0"));
     keyCount++;
 
-    console.log("Here we are in previous-search.service @ save");
-    console.log("The index of the search term recieved is " + keyCount);
-    console.log("And the search we are saving is " + searchString);
+    console.log("Saving search to localStorage: " + searchString);
     this.storage.set("0", String(keyCount));
     this.storage.set(String(keyCount), searchString);
   }
 
 
   //get function that gets all the searches in localStorage
-  public getFromLocal(): void {
-    console.log("Here we are in previous-search.service @ get");
-    console.log("And the values in localStorage are: ");
+  public getFromLocal():ParrotSearch[] {
+    console.log("Retrieving values from localStorage: ");
 
     var searchArray: ParrotSearch[] = [];
     //get the number of search results from localStorage
-    //var numSearches = parseInt(this.storage.get("0"));
+    var numSearches = parseInt(this.storage.get("0"));
 
     //loop through the localStorage and save each search in the array
-    for(var i = 0; i <= keyCount; i++) {
+    for(var i = 0; i <= numSearches; i++) {
       let searchJSON = JSON.parse( this.storage.get(String(i)) );
       searchArray.push(searchJSON);
       console.log( i + ": " + this.storage.get(String(i)) );
     }
-
+    
+    return searchArray;
   }
 }
