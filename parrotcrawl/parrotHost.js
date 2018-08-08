082ab0b753347980ac3ad470aa7762804297977d
CharlesLedbetter <32339141+CharlesLedbetter@users.noreply.github.com>
2018-08-08 17:54:34 -0500

    added parrotHost.js

---
+++ parrotcrawl/parrotHost.js
@@ -0,0 +1,18 @@
+//--------  Server Requirements ---------//
+const express = require('express');
+const path  = require('path');
+const VIEWS = path.join(__dirname);
+const app = express();
+
+
+//------------  Server Routes ------------//
+app.listen('16178', () => {
+  console.log('ngxServe started!');
+});
+
+app.use(express.static('dist/ParrotCrawler'));
+
+
+app.get('/*', function(req,res){
+  res.sendFile('index.html', { root : VIEWS });
+})
